d923ee78f1204ab8f041cf759bc5ab437835567a
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-04-14 13:07:16 +0530

    Fetching data on server with props

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,29 @@
 import React from "react"
 import axios from "axios"
 
-function Home() {
-  React.useEffect(() => {
-    getProducts()
-  }, [])
+function Home(products) {
+  console.log("props", products)
+  // React.useEffect(() => {
+  //   getProducts()
+  // }, [])
 
-  async function getProducts(){
-    const url = "http://localhost:3000/api/products"
-    const response = await axios.get(url)
-    console.log("----------", response.data)
-  }
+  // async function getProducts(){
+  //   const url = "http://localhost:3000/api/products"
+  //   const response = await axios.get(url)
+  //   console.log("----------", response.data)
+  // }
 
   return <>home</>;
+}
+
+Home.getInitialProps = async () => {
+    //fetch data on server
+    const url = "http://localhost:3000/api/products"
+    const response = await axios.get(url)
+    return {products: response.data}
+    //return resonse data as an object
+   
+    //note: this object will be merged with eisting props
 }
 
 export default Home;
