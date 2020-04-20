12e9b607569c635a2ca63ee4be97738795208826
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-04-20 11:01:36 +0530

    Prevent CatchErrors

--- pages/api/product.js
+++ pages/api/product.js
@@ -1,16 +1,16 @@
 import Product from "../../models/Product";
-import connectDb from "../../utils/connectDb"
+import connectDb from "../../utils/connectDb";
 
-connectDb()
+connectDb();
 
 export default async (req, res) => {
   switch (req.method) {
     case "GET":
       await handleGetRequest(req, res);
-	  break;
-	case "POST":
-	   await handlePostRequest(req, res)
-	   break;
+      break;
+    case "POST":
+      await handlePostRequest(req, res);
+      break;
     case "DELETE":
       await handleDeleteRequest(req, res);
       break;
@@ -27,17 +27,22 @@ async function handleGetRequest(req, res) {
 }
 
 async function handlePostRequest(req, res) {
-	const {name, price, description, mediaUrl} = req.body
-	if(!name || !price || !description || !mediaUrl) {
-		return res.status(422).send("Product missing one or more fields")
-	}
-	const product = await new Product({
-		name,
-		price,
-		description,
-		mediaUrl
-	}).save()
-	res.status(201).json(product)
+  const { name, price, description, mediaUrl } = req.body;
+  try {
+    if (!name || !price || !description || !mediaUrl) {
+      return res.status(422).send("Product missing one or more fields");
+    }
+    const product = await new Product({
+      name,
+      price,
+      description,
+      mediaUrl
+    }).save();
+    res.status(201).json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error in creating product");
+  }
 }
 
 async function handleDeleteRequest(req, res) {
@@ -45,7 +50,6 @@ async function handleDeleteRequest(req, res) {
   await Product.findOneAndDelete({ _id });
   res.status(204).json({});
 }
-
 
 // export default async (req, res) => {
 // 	const {_id} = req.query
