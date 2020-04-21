ad6112fb3092ce176cc2e9cc3e86de1fd0634341
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-04-21 14:40:29 +0530

    Signup user with JWT Authentication

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   env: {
     MONGO_SRV: "mongodb+srv://admin:abc$123@reactreserve-hfzoc.mongodb.net/test?retryWrites=true&w=majority",
-    JWT_SECRET: "<insert-jwt-secret>",
+    JWT_SECRET: "helloworld$$$",
     CLOUDINARY_URL: "http://api.cloudinary.com/v1_1/cloudnine-hospitals/image/upload",
     STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
   }
